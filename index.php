537caf9f7a026e40559de068c4ae8778edff308d
LeaOolala <degli.esposti.lea@gmail.com>
2025-02-26 16:12:49 +0100

    réorganisation

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    include 'theme_parts/header.php';
+?>
--- theme_parts/menu.php
+++
@@ -1,5 +0,0 @@
-<?php echo"<nav>" ;?>
-    <?php echo"<./page_accueil.php>" ;?>
-    <?php echo"./page_programmation.php" ;?>
-    <?php echo"<./page_contact.php>" ;?>
-<?php echo"</nav>" ;?>
